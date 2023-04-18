0f735bf23796e5981bb4f56cf5be6ef500558863
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2023-04-18 16:10:01 +0700

    memasukan semua rank 8Kyu kedalam package

--- src/eightkyu/SumOfPostive.kt
+++ src/eightkyu/SumOfPostive.kt
@@ -1,3 +1,10 @@
+package eightkyu
+
+/**
+ * You get an array of numbers, return the sum of all of the positives ones.
+ * Example [1,-4,7,12] => 1 + 7 + 12 = 20
+ * Note: if there is nothing to sum, the sum is default to 0.
+ */
 fun sum(numbers: IntArray): Int {
     var total = 0
 
