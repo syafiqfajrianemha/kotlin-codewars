ff2fe9e4ab1d1048a5006b90d1f822416dfcfec2
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2023-05-12 14:50:02 +0700

    8kyu - Find the smallest integer in the array

---
+++ src/eightkyu/FindTheSmallestIntegerInTheArray.kt
@@ -0,0 +1,25 @@
+package eightkyu
+
+/*
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+- Given [34, 15, 88, 2] your solution will return 2
+- Given [34, -345, -1, 100] your solution will return -345
+
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+ */
+fun findSmallestInt(nums: List<Int>): Int {
+    var minimalNum = nums[0]
+    for(num in nums){
+        if(num < minimalNum){
+            minimalNum = num
+        }
+    }
+    return minimalNum
+}
+
+fun main() {
+     println(findSmallestInt(listOf(34, 15, 88, 2)))
+     println(findSmallestInt(listOf(34, -345, -1, 100)))
+}
